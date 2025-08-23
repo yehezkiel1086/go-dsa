d18b8d77d2c9f17f1183dca834733de770e7dcba
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-08-23 17:39:28 +0700

    feat(data-structures): add ds-linked-lists

--- ds-linked-lists/main.go
+++ ds-linked-lists/main.go
@@ -1,76 +1,124 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 /**
 Traversal
+Find lowest value
 Remove a node
 Insert a node
 Sort
 */
 
-type t_node interface {
-	traverse()
-	findLowest() *Node
-	deleteNode(*Node, *Node) *Node
-}
-
 type Node struct {
-	data int
-	next *Node
+	Data int
+	Next *Node
 }
 
-func (node *Node) traverse() {
-	for curr := node; curr != nil; curr = curr.next {
-		fmt.Print(curr.data, " -> ")
+func (n *Node) traverse() {
+	for n != nil {
+		fmt.Printf("%v -> ", n.Data)
+		n = n.Next
 	}
-	fmt.Println("null")
+	fmt.Println("nil")
 }
 
-func (node *Node) findLowest() (*Node) {
-	var lowest *Node
-	lowest = node
-	for tmp := node; tmp.next != nil; tmp = tmp.next {
-		if tmp.data < lowest.data {
-			lowest = tmp
+func (n *Node) findLowest() int {
+	lowest := n.Data
+	n = n.Next
+
+	for n != nil {
+		if n.Data < lowest {
+			lowest = n.Data
 		}
+		n = n.Next
 	}
+
 	return lowest
 }
 
-func (node *Node) deleteNode(head *Node, nodeToDelete *Node) *Node {
-	if head == nodeToDelete {
-		return head.next
+func (head *Node) deleteNode(node *Node) *Node {
+	// if node to delete is head
+	if head == node {
+		return head.Next
 	}
 
-	currNode := head
-	for (currNode.next != nil && currNode.next != nodeToDelete) {
-		currNode = currNode.next
+	// find node to delete
+	curr := head
+	for curr.Next != nil && curr.Next != node {
+		curr = curr.Next
 	}
 
-	if currNode.next == nil {
+	// if no node to delete
+	if curr.Next == nil {
 		return head
 	}
 
-	currNode.next = currNode.next.next
+	// if node to delete is found
+	curr.Next = curr.Next.Next
 
 	return head
 }
 
+func (head *Node) insert(node *Node, pos int) *Node {
+	// if position == 1
+	if pos == 1 {
+		node.Next = head
+		return node
+	}
+
+	// traverse
+	curr := head
+	for i := 0; i < pos - 1 && curr != nil; i++ {
+		curr = curr.Next
+	}
+
+	if curr == nil {
+		return head
+	}
+
+	// assign new node
+	node.Next = curr.Next
+	curr.Next = node
+	return head
+}
+
 func main() {
-	head := &Node{data: 5}
-	head = &Node{data: 2, next: head}
-	head = &Node{data: 4, next: head}
-	head = &Node{data: 1, next: head}
-	head = &Node{data: 3, next: head}
+	node5 := &Node{
+		Data: 9,
+	}
+	node4 := &Node{
+		Data: 2,
+		Next: node5,
+	}
+	node3 := &Node{
+		Data: 3,
+		Next: node4,
+	}
+	node2 := &Node{
+		Data: 11,
+		Next: node3,
+	}
+	head := &Node{
+		Data: 7,
+		Next: node2,
+	}
 
-	var list t_node = head
+	// traverse
+	head.traverse()
 
-	list.traverse()
-	fmt.Println(list.findLowest())
+	// find lowest
+	lowest := head.findLowest()
+	fmt.Println(lowest)
 
-	list = list.deleteNode(head, head.next)
-	list.traverse()
+	// delete node
+	head = head.deleteNode(node3)
+	head.traverse()
+
+	// insert node
+	newNode := &Node{
+		Data: 10,
+	}
+	head = head.insert(newNode, 3)
+	head.traverse()
 }
